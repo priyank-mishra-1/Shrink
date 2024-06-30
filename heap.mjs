9b61989c76389e097a6319719b51a83f8a7be694
Priyank Kumar Mishra <priyank.21gcebit016@galgotiacollege.edu>
2024-07-01 02:51:52 +0530

    created huffman tree

---
+++ heap.mjs
@@ -0,0 +1,68 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  add(node) {
+    this.heap.push(node);
+    this._heapifyUp(this.heap.length - 1);
+  }
+
+  remove() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._heapifyDown(0);
+    return root;
+  }
+
+  _heapifyUp(index) {
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index]._weight() < this.heap[parentIndex]._weight()) {
+        [this.heap[index], this.heap[parentIndex]] = [
+          this.heap[parentIndex],
+          this.heap[index],
+        ];
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  _heapifyDown(index) {
+    const length = this.heap.length;
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let smallest = index;
+
+      if (
+        leftChildIndex < length &&
+        this.heap[leftChildIndex]._weight() < this.heap[smallest]._weight()
+      ) {
+        smallest = leftChildIndex;
+      }
+      if (
+        rightChildIndex < length &&
+        this.heap[rightChildIndex]._weight() < this.heap[smallest]._weight()
+      ) {
+        smallest = rightChildIndex;
+      }
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[index],
+      ];
+      index = smallest;
+    }
+  }
+
+  get length() {
+    return this.heap.length;
+  }
+}
+
+export default MinHeap;
---
+++ huffman.mjs
@@ -0,0 +1,110 @@
+import MinHeap from "./heap.mjs";
+
+class HuffmanBaseNode {
+  _isLeaf() {}
+  _weight() {}
+}
+
+class HuffLeafNode extends HuffmanBaseNode {
+  constructor(el, wt) {
+    super(); // Assuming HuffmanBaseNode is a class with a constructor
+    this.element = el;
+    this.weight = wt;
+  }
+  value() {
+    return this.element;
+  }
+  _weight() {
+    return this.weight; // returns weight not weight() unless weight is not defined
+  }
+  _isLeaf() {
+    return true;
+  }
+}
+
+class HuffInternalNode extends HuffmanBaseNode {
+  constructor(l, r, wt) {
+    super(); // Assuming HuffmanBaseNode is a class with a constructor
+    this.left = l;
+    this.right = r;
+    this.weight = wt;
+  }
+  _left() {
+    return this.left;
+  }
+  _right() {
+    return this.right;
+  }
+  _weight() {
+    return this.weight;
+  }
+  _isLeaf() {
+    return false;
+  }
+}
+
+export class HuffTree {
+  constructor(elOrL, wtOrR, wt) {
+    if (typeof elOrL === "undefined") {
+      throw new Error("Invalid arguments");
+    }
+
+    if (typeof wt === "undefined") {
+      // Only two arguments, assume it's a leaf node
+      this.root = new HuffLeafNode(elOrL, wtOrR);
+    } else {
+      // Three arguments, assume it's an internal node
+      this.root = new HuffInternalNode(elOrL, wtOrR, wt);
+    }
+  }
+
+  _root() {
+    return this.root;
+  }
+
+  _weight() {
+    return this.root._weight();
+  }
+
+  compareTo(that) {
+    if (this._weight() < that._weight()) {
+      return -1;
+    } else if (this._weight() === that._weight()) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+}
+
+export function buildHuffmanTree(Hheap) {
+  let temp1,
+    temp2,
+    temp3 = null;
+  while (Hheap.length > 1) {
+    temp1 = Hheap.remove();
+    temp2 = Hheap.remove();
+    temp3 = new HuffTree(
+      temp1._root(),
+      temp2._root(),
+      temp1._weight() + temp2._weight()
+    );
+    Hheap.add(temp3);
+  }
+  return temp3;
+}
+
+// DFS
+export function generateCodes(node, prefix = "", codes = {}) {
+  if (node._isLeaf()) {
+    codes[node.value()] = prefix;
+  } else {
+    if (node._left()) {
+      generateCodes(node._left(), prefix + "0", codes);
+    }
+    if (node._right()) {
+      generateCodes(node._right(), prefix + "1", codes);
+    }
+  }
+  return codes;
+}
